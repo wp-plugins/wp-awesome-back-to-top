1f642a1589960b5aa5e72301ca5634e02c14962a
suifengtec <suifengtec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-01 03:09:39 +0000

    plugin files
    git-svn-id: https://plugins.svn.wordpress.org/wp-awesome-back-to-top/trunk@958431 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/class.settings-api.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * Settings API wrapper class
+ */
+if ( !class_exists( 'AB2T_Settings_API' ) ):
+class AB2T_Settings_API {
+
+    private $settings_sections = array();
+
+    private $settings_fields = array();
+
+    private static $_instance;
+    public function __construct() {
+
+    }
+
+    function set_sections( $sections ) {
+        $this->settings_sections = $sections;
+
+        return $this;
+    }
+
+    function add_section( $section ) {
+        $this->settings_sections[] = $section;
+
+        return $this;
+    }
+
+    function set_fields( $fields ) {
+        $this->settings_fields = $fields;
+
+        return $this;
+    }
+
+    function add_field( $section, $field ) {
+        $defaults = array(
+            'name' => '',
+            'label' => '',
+            'desc' => '',
+            'type' => 'text'
+        );
+
+        $arg = wp_parse_args( $field, $defaults );
+        $this->settings_fields[$section][] = $arg;
+
+        return $this;
+    }
+
+    function admin_init() {
+        //register settings sections
+        foreach ( $this->settings_sections as $section ) {
+            if ( false == get_option( $section['id'] ) ) {
+                add_option( $section['id'] );
+            }
+
+            if ( isset($section['desc']) && !empty($section['desc']) ) {
+                $section['desc'] = '<div class="inside">'.$section['desc'].'</div>';
+                $callback = create_function('', 'echo "'.str_replace('"', '\"', $section['desc']).'";');
+            } else {
+                $callback = '__return_false';
+            }
+
+            add_settings_section( $section['id'], $section['title'], $callback, $section['id'] );
+        }
+
+        //register settings fields
+        foreach ( $this->settings_fields as $section => $field ) {
+            foreach ( $field as $option ) {
+
+                $type = isset( $option['type'] ) ? $option['type'] : 'text';
+
+                $args = array(
+                    'id' => $option['name'],
+                    'desc' => isset( $option['desc'] ) ? $option['desc'] : '',
+                    'name' => $option['label'],
+                    'section' => $section,
+                    'size' => isset( $option['size'] ) ? $option['size'] : null,
+                    'options' => isset( $option['options'] ) ? $option['options'] : '',
+                    'std' => isset( $option['default'] ) ? $option['default'] : '',
+                    'sanitize_callback' => isset( $option['sanitize_callback'] ) ? $option['sanitize_callback'] : '',
+                );
+                add_settings_field( $section . '[' . $option['name'] . ']', $option['label'], array( $this, 'callback_' . $type ), $section, $section, $args );
+            }
+        }
+
+        // creates our settings in the options table
+        foreach ( $this->settings_sections as $section ) {
+            register_setting( $section['id'], $section['id'], array( $this, 'sanitize_options' ) );
+        }
+    }
+
+    function callback_text( $args ) {
+
+        $value = esc_attr( $this->get_option( $args['id'], $args['section'], $args['std'] ) );
+        $size = isset( $args['size'] ) && !is_null( $args['size'] ) ? $args['size'] : 'regular';
+
+        $html = sprintf( '<input type="text" class="%1$s-text" id="%2$s[%3$s]" name="%2$s[%3$s]" value="%4$s"/>', $size, $args['section'], $args['id'], $value );
+        $html .= sprintf( '<span class="description"> %s</span>', $args['desc'] );
+
+        echo $html;
+    }
+
+    function callback_checkbox( $args ) {
+
+        $value = esc_attr( $this->get_option( $args['id'], $args['section'], $args['std'] ) );
+
+        $html = sprintf( '<input type="hidden" name="%1$s[%2$s]" value="off" />', $args['section'], $args['id'] );
+        $html .= sprintf( '<input type="checkbox" class="checkbox" id="%1$s[%2$s]" name="%1$s[%2$s]" value="on"%4$s />', $args['section'], $args['id'], $value, checked( $value, 'on', false ) );
+        $html .= sprintf( '<label for="%1$s[%2$s]"> %3$s</label>', $args['section'], $args['id'], $args['desc'] );
+
+        echo $html;
+    }
+
+
+    function sanitize_options( $options ) {
+        foreach( $options as $option_slug => $option_value ) {
+            $sanitize_callback = $this->get_sanitize_callback( $option_slug );
+
+            // If callback is set, call it
+            if ( $sanitize_callback ) {
+                $options[ $option_slug ] = call_user_func( $sanitize_callback, $option_value );
+                continue;
+            }
+
+            // Treat everything that's not an array as a string
+            if ( !is_array( $option_value ) ) {
+                $options[ $option_slug ] = sanitize_text_field( $option_value );
+                continue;
+            }
+        }
+        return $options;
+    }
+
+    function get_sanitize_callback( $slug = '' ) {
+        if ( empty( $slug ) )
+            return false;
+        // Iterate over registered fields and see if we can find proper callback
+        foreach( $this->settings_fields as $section => $options ) {
+            foreach ( $options as $option ) {
+                if ( $option['name'] != $slug )
+                    continue;
+                // Return the callback name
+                return isset( $option['sanitize_callback'] ) && is_callable( $option['sanitize_callback'] ) ? $option['sanitize_callback'] : false;
+            }
+        }
+        return false;
+    }
+
+    function get_option( $option, $section, $default = '' ) {
+
+        $options = get_option( $section );
+
+        if ( isset( $options[$option] ) ) {
+            return $options[$option];
+        }
+
+        return $default;
+    }
+
+    function show_navigation() {
+        $html = '<h2 class="nav-tab-wrapper">';
+
+        foreach ( $this->settings_sections as $tab ) {
+            $html .= sprintf( '<a href="#%1$s" class="nav-tab" id="%1$s-tab">%2$s</a>', $tab['id'], $tab['title'] );
+        }
+
+        $html .= '</h2>';
+
+        echo $html;
+    }
+
+    function show_forms() {
+        ?>
+        <div class="metabox-holder">
+            <div class="postbox">
+                <?php foreach ( $this->settings_sections as $form ) { ?>
+                    <div id="<?php echo $form['id']; ?>" class="group">
+                        <form method="post" action="options.php">
+
+                            <?php do_action( 'wsa_form_top_' . $form['id'], $form ); ?>
+                            <?php settings_fields( $form['id'] ); ?>
+                            <?php do_settings_sections( $form['id'] ); ?>
+                            <?php do_action( 'wsa_form_bottom_' . $form['id'], $form ); ?>
+
+                            <div style="padding-left: 10px">
+                                <?php submit_button(); ?>
+                            </div>
+                        </form>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        <?php
+     $this->script();
+    }
+
+    function script() {
+        ?>
+        <style type="text/css">
+            .form-table th { padding: 20px 10px; }
+            #wpbody-content .metabox-holder { padding-top: 5px; }
+        </style>
+        <?php
+    }
+
+}
+endif;
